16f9dfd06430cb815eddc797d5b3e977ea6a7e0c
Dominic <dominic.rossi@uconn.edu>
2026-02-09 15:26:09 -0500

    Completed hw 2

--- setup_env.py
+++ setup_env.py
@@ -8,8 +8,12 @@ ENV_DIR = Path("3000-env")
 # Create venv
 venv.create(ENV_DIR, with_pip=True)
 
-pip = ENV_DIR / "bin" / "pip"
-python = ENV_DIR / "bin" / "python"
+if sys.platform == "win32":
+    pip = ENV_DIR / "Scripts" / "pip.exe"
+    python = ENV_DIR / "Scripts" / "python.exe"
+else:
+    pip = ENV_DIR / "bin" / "pip"
+    python = ENV_DIR / "bin" / "python"
 
 packages = [
     "numpy==1.26.4",
